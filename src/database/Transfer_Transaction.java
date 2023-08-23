d2077c8e49f6fec5ce9a6efa65644d2e45be8972
Sugarlolo <jih8680@naver.com>
2023-08-23 17:49:05 +0900

    트랜잭션 구현중

--- src/database/Transfer_Transaction.java
+++ src/database/Transfer_Transaction.java
@@ -3,42 +3,77 @@ package database;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import beans.MemberBean;
+import beans.TransferBean;
 import database.DBConnectionMgr;
 
 public class Transfer_Transaction {
 	
 	private DBConnectionMgr pool;
-
+	
+//	TransferBean tb = 
 	public Transfer_Transaction () {
 		pool = DBConnectionMgr.getInstance();
 	}
 	
-	public void Transaction_Do() {
+	public void Transaction_Do(int balance) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		TransferBean tb = new TransferBean();
+		String currentTimestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+		try {
+			con = pool.getConnection();
+			sql = "INSERT INTO TRANSFER (TRANSFER_DO_ACCOUNT,"
+					+ "TRANSFER_BALANCE,"
+					+ "TRANSFER_TAKE_ACCOUNT,"
+					+ "TRANSFER_DATE,"
+					+ "TRANSFER_CATEGORY,"
+					+ "TRANSFER_MEMO,"
+					+ "TRANSFER_DO_BALANCE,"
+					+ "TRANSFER_TAKE_BALANCE "
+					+ "values(?,?,?,?,?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1,tb.getTransfer_Do_Account());
+			pstmt.setInt(2,tb.getTransfer_Balance());
+			pstmt.setInt(3,tb.getTransfer_Take_Account());
+			pstmt.setString(4, currentTimestamp);
+			pstmt.setString(5, )
+			
+			int cnt = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return;
+	}
+	
+	public void Transaction_Take() {
+		
+	}
+	
+	public boolean Transaction_Check() {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql = null;
 		try {
 			con = pool.getConnection();
-			sql = "UPDATE ";
+			sql = "SELECT ";
 			pstmt = con.prepareStatement(sql);
 			rs = pstmt.executeQuery();
-			
-			con.setAutoCommit(false);
-			
-			while(rs.next()) {
-				
-			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			pool.freeConnection(con, pstmt, rs);
 		}
 		return;
-	}
-	
-	public void Transaction_Take() {
 		
 	}
 	
